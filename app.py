3095eac82bc82c59e35ae4776c79b4a0b93f377e
Otabek Rizayev <46789195+Otabek-Rizayev@users.noreply.github.com>
2024-12-26 09:47:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ if file:
   pred, pred_id, probs = model.predict(img)
   if int(probs[pred_id]*100) > 70:
     st.success(f"Topdim! Bu:  {pred}")
-    st.info(f"<{pred}> bo'lish Ehtimolligi: {probs[pred_id]*100:1f}%")
+    st.info(f"<{pred}> bo'lish Ehtimolligi: {probs[pred_id]*100:..1f}%")
     
     # plotting (grafik)
     fig = px.bar(x=probs*100, y=model.dls.vocab)
