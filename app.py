7cc64bc0e62df51808dd9dfb3dec19d6bab08509
Otabek Rizayev <46789195+Otabek-Rizayev@users.noreply.github.com>
2024-12-26 09:39:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if file:
   model = load_learner("cnn-classification-model.pkl")
   # prediction (bashorat)
   pred, pred_id, probs = model.predict(img)
-  if int(math.sqrt(probs[pred_id])) > 90:
+  if int(probs[pred_id]) > 80:
     st.success(f"Topdim! Bu:  {pred}")
     st.info(f"<{pred}> bo'lish Ehtimolligi: {probs[pred_id]*100}%")
     
